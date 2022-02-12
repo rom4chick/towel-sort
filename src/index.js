3ce623e4e155320e38496ae73b7f9c3a0f3a415d
rom4chick <obereg2005.2002@gmail.com>
2022-02-12 10:35:57 +0300

    init: init commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let towelArr = [];
+  if (matrix) {
+    matrix.forEach((arr, index) => {
+      if (index % 2 === 0) {
+        arr.forEach(arrEl => towelArr.push(arrEl));
+      } else {
+        arr.reverse();
+        arr.forEach(arrEl => {
+          towelArr.push(arrEl);
+        })
+     
+      } 
+    });
+  }
+  
+  return towelArr;
 }
